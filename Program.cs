cca1813bf1e6c7d44dfc7aac58f68a26ffb7249d
EricShanks68 <e-ricshanks@hotmail.com>
2021-05-17 16:33:57 -0700

    fizzbuzz program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace FizzBuzz
 {
@@ -6,7 +7,32 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            fizzbuzz(100);
+
+        }
+
+        // Simple fizzbuzz method. Prints Fizz if divisible by 3, buzz if divisible by 5.
+        public static ArrayList fizzbuzz(int n)
+        {
+            ArrayList answer = new ArrayList();
+            for (int i = 1; i < n; i++)
+            {
+                String output = "";
+                if (i % 3 == 0)
+                {
+                    output += "fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "buzz";
+                }
+                else if (output == "") { output = i.ToString(); }
+                answer.Add(output);
+                Console.WriteLine(output);
+            }
+            //Console.WriteLine(answer); this does not print values of the array list dummy
+            return answer;
         }
     }
 }
+
